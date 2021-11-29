6b77dc6c668e33baf944cb007ef4afdc4e66aa51
hernandesmacedo <hcarneiromacedo@gmail.com>
2021-11-29 18:52:49 -0300

    Queue class created

--- class.py
+++ class.py
@@ -7,3 +7,19 @@ class PCB:
         self.init_end_address = init_end_address
         self.remaining_time = remaining_time
         self.next = next
+
+class Queue:
+    def __init__(self):
+        self.first = None
+
+    def context_switch(self, quantum):
+        self.first.remaining_time -= quantum
+        self.first = self.first.next
+
+    def process_end(self):
+        aux = self.first
+        self.first = self.first.next
+        del aux
+
+    def queue_end(self):
+        del self
