715de8e87296ef4ab0b815e4d8cdd27117cec4f8
hernandesmacedo <hcarneiromacedo@gmail.com>
2021-11-30 01:26:10 -0300

    Changed to a more dynamic processes_creator

--- main.py
+++ main.py
@@ -1,57 +1,27 @@
 import random
 from datetime import datetime, timedelta
-from classes import PCB, Queue
+from classes import PCB, Queue, Scheduler
 
-def processes_creator(top_priority_queue, second_priority_queue, third_priority_queue, last_priority_queue):
+def processes_creator(scheduler):
+    
     end_address = 9999
     date_time = datetime.now()
+    number_of_processes = 0
 
-    for i in range(1, 7):
-        name = 'Processo ' + str(i)
-        priority = 4
-        date_time = date_time + timedelta(minutes=1)
+    while len(scheduler.ordered_queues) != 4 or number_of_processes < 19:
+        name = 'Processo ' + str(number_of_processes + 1)
+        priority = random.randrange(1, 5)
+        date_time += timedelta(minutes=1)
         init_address = end_address + 1
         end_address = init_address + random.randrange(0, 20)
-        remaining_time = random.randrange(1, 12)
+        remaining_time = random.randrange(2, 23)
         process = PCB(name, priority, date_time, [init_address, end_address], remaining_time)
-        top_priority_queue.new_process(process)
-
-    for i in range(7, 12):
-        name = 'Processo ' + str(i)
-        priority = 3
-        date_time = date_time + timedelta(minutes=1)
-        init_address = end_address + 1
-        end_address = init_address + random.randrange(0, 20)
-        remaining_time = random.randrange(1, 12)
-        process = PCB(name, priority, date_time, [init_address, end_address], remaining_time)
-        second_priority_queue.new_process(process)
-
-    for i in range(12, 16):
-        name = 'Processo ' + str(i)
-        priority = 2
-        date_time = date_time + timedelta(minutes=1)
-        init_address = end_address + 1
-        end_address = init_address + random.randrange(0, 20)
-        remaining_time = random.randrange(1, 12)
-        process = PCB(name, priority, date_time, [init_address, end_address], remaining_time)
-        third_priority_queue.new_process(process)
-
-    for i in range(16, 20):
-        name = 'Processo ' + str(i)
-        priority = 1
-        date_time = date_time + timedelta(minutes=1)
-        init_address = end_address + 1
-        end_address = init_address + random.randrange(0, 20)
-        remaining_time = random.randrange(1, 12)
-        process = PCB(name, priority, date_time, [init_address, end_address], remaining_time)
-        last_priority_queue.new_process(process)
+        scheduler.schedule_process(process)
+        number_of_processes += 1
 
 def main():
-    top_priority_queue = Queue(quantum = 4)
-    second_priority_queue = Queue(quantum = 3)
-    third_priority_queue = Queue(quantum = 2)
-    last_priority_queue = Queue(quantum = 2)
-
-    processes_creator(top_priority_queue, second_priority_queue, third_priority_queue, last_priority_queue)
-
+    scheduler = Scheduler()
+    
+    processes_creator(scheduler)
+            
 main() 
