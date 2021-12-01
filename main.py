88899b5089fd518d76c51fd468e1c8539ca2a0cc
hernandesmacedo <hcarneiromacedo@gmail.com>
2021-12-01 18:19:41 -0300

    Switched from randrange to randint just for better visualization

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ def processes_creator(scheduler):
 
     while len(scheduler.ordered_queues) != 4 or number_of_processes < 18:
         name = 'Process {:02d}'.format(number_of_processes + 1)
-        priority = random.randrange(1, 5)
+        priority = random.randint(1, 4)
         date_time += timedelta(minutes=1)
         init_address = end_address + 1
-        end_address = init_address + random.randrange(0, 20)
-        remaining_time = random.randrange(2, 18)
+        end_address = init_address + random.randint(0, 20)
+        remaining_time = random.randint(2, 18)
         process = PCB(name, priority, date_time, [init_address, end_address], remaining_time)
         scheduler.schedule_process(process)
         number_of_processes += 1
