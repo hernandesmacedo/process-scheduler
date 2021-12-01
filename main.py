2048767263a920d46b5490ffce0c65036aa36dd9
hernandesmacedo <hcarneiromacedo@gmail.com>
2021-11-30 23:45:47 -0300

    2-digits process number and decreased remaining_time

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ def processes_creator(scheduler):
     number_of_processes = 0
 
     while len(scheduler.ordered_queues) != 4 or number_of_processes < 18:
-        name = 'Processo ' + str(number_of_processes + 1)
+        name = 'Process {:02d}'.format(number_of_processes + 1)
         priority = random.randrange(1, 5)
         date_time += timedelta(minutes=1)
         init_address = end_address + 1
         end_address = init_address + random.randrange(0, 20)
-        remaining_time = random.randrange(2, 23)
+        remaining_time = random.randrange(2, 18)
         process = PCB(name, priority, date_time, [init_address, end_address], remaining_time)
         scheduler.schedule_process(process)
         number_of_processes += 1
